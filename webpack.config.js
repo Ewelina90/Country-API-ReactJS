01cbcc95215a336ffeb6b65b85a1312c23738b48
Ewelina Dziura <ewedziura@gmail.com>
2017-10-08 17:37:14 +0200

    initial files

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+//Konfiguracja Webpack
+module.exports = {
+    entry: "./js/index.jsx",
+    output: { filename: "./js/app.js" },
+    watch: true,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,  exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: { presets: ['es2015', 'stage-2' , 'react'] }
+            }
+        ]
+    }
+}
